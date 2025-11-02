5414fb04a7af3e46aa31842e2fd9beff5cd48a54
Bismark Aikins <aikinsb97@gmail.com>
2025-11-02 22:38:02 +0000

    initial commit

---
+++ app/routers/security/admin_authentication.py
@@ -0,0 +1,75 @@
+from datetime import datetime, timedelta, timezone
+
+import jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jwt.exceptions import InvalidTokenError
+from pwdlib import PasswordHash
+from sqlalchemy.orm import Session
+from ...schemas import UserSchema, TokenData, Settings
+from ...crud import get_admin_by_name
+from ...dependencies import get_db
+
+
+
+settings = Settings()
+
+
+
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+password_hash = PasswordHash.recommended()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")  
+
+
+def get_password_hash(plain_password: str):
+    return password_hash.hash(plain_password)
+
+def verify_password(plain_password: str, hashed_password):
+    return password_hash.verify(plain_password, hashed_password)
+
+def authenticate_admin(db: Session, username: str, password: str):
+    user = get_admin_by_name(db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def get_current_admin(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, settings.secret_key, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if not username:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_admin_by_name(db, token_data.username)
+    if not user:
+        raise credentials_exception
+    return user
+
+async def get_current_active_admin(
+    current_user: UserSchema = Depends(get_current_admin),
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
