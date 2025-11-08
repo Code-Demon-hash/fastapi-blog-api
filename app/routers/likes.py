66e0c6c0a99168eb96592e3c64e4a5a0753d56ce
Bismark Aikins <aikinsb97@gmail.com>
2025-11-08 16:59:26 +0000

    Updated likes

--- app/routers/likes.py
+++ app/routers/likes.py
@@ -1,34 +1,29 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy import select
 from sqlalchemy.orm import Session
-from ..models import Likes
-from ..schemas import LikeCreate, ReadLikes
+from ..models import Likes, Blogs
+from ..crud import create_like_post
+from ..schemas import PostLike, ReadLikes
+from .security.user_authentication import get_current_active_user
 from ..dependencies import get_db
 
 
 router = APIRouter(prefix="/comments", tags=["comments"])
 
 
-@router.post("/like")
-def like_content(like: LikeCreate, db: Session = Depends(get_db)):
-    liked_already = db.execute(select(Likes).where(Likes.user_id == like.user_id)),
-    ((Likes.blog_id == like.blog_id) | (Likes.comment_id == like.comment_id
-                               )).scalar_one_or_none()
-    if liked_already:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                            detail = "Already liked this content")
-    create_new_like_content = Likes(like.model_dump()) 
-    db.add(create_new_like_content)
-    db.commit()
-    db.refresh(create_new_like_content)
-    return create_new_like_content                  
+@router.post("/like", response_model=ReadLikes)
+async def like_post(like: PostLike,
+                 current_user = Depends(get_current_active_user), 
+                 db: Session = Depends(get_db)):
+    post = db.execut(select(Blogs).where(Blogs.id == like.blog_id)).scalar_one_or_none()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail = "Blog not found")
+    user_like_post = create_like_post(db, like, current_user.user_name)
+    return user_like_post
+          
 
 @router.get("/likes/blog/{blog_id}", response_model=ReadLikes)
-def get_likes_for_blog(blog_id: int, db: Session = Depends(get_db)):
+def get_blog_with_likes(blog_id: int, db: Session = Depends(get_db)):
     likes = db.execute(select(Likes).where(Likes.blog_id == blog_id)).scalar_one_or_none()
-    return likes
-
-@router.get("/likes/comment/{comment_id}", response_model=ReadLikes)
-def get_likes_for_comment(comment_id: int, db: Session = Depends(get_db)):
-    likes = db.execute(select(Likes).where(Likes.comment_id == comment_id)).scalar_one_or_none()
     return likes
