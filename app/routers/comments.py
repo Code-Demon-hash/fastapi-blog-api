7c9e503ab2a2e9f5e56cc623e71131538e9219a3
Bismark Aikins <aikinsb97@gmail.com>
2025-11-14 12:36:47 +0000

    modified

--- app/routers/comments.py
+++ app/routers/comments.py
@@ -2,10 +2,9 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 from ..models import Blogs
-from ..schemas import CommentCreate, CommentRead, ReadLikes
+from ..schemas import CommentCreate, CommentRead
 from ..crud import create_comment
 from ..dependencies import get_db
-from .security.user_authentication import get_current_active_user
 
 
 
@@ -14,13 +13,13 @@ router = APIRouter(prefix="/comments", tags=["comments"])
 
 @router.post("/comments/", status_code=status.HTTP_201_CREATED)
 async def create_comment_route(comment: CommentCreate,
-                         db: Session = Depends(get_db),
-                         current_user = Depends(get_current_active_user)):
+                         db: Session = Depends(get_db)
+):
     post = db.execute(select(Blogs).where(Blogs.id == comment.blog_id)).scalar_one_or_none()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="Blog does not exist")
-    comment_service = create_comment(db, comment, current_user.username)
+    comment_service = create_comment(db, comment)
     return comment_service
 
 
