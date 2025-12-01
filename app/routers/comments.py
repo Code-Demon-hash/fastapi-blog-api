6c224b058914e84263612320630f146a2b61f547
Bismark Aikins <aikinsb97@gmail.com>
2025-12-01 11:45:47 +0000

    modified

--- app/routers/comments.py
+++ app/routers/comments.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from typing import List
 from sqlalchemy import select
 from sqlalchemy.orm import Session
-from ..models import Blogs
-from ..schemas import CommentCreate, CommentRead
-from ..crud import create_comment
+from ..models import Blogs, Comments
+from .security.user_authentication import get_current_user
+from ..schemas import CommentCreate, CommentRead, BlogStatus
+from ..crud import create_comment, get_comment_by_blog_id
 from ..dependencies import get_db
 
 
@@ -11,22 +13,20 @@ from ..dependencies import get_db
 router = APIRouter(prefix="/comments", tags=["comments"])
 
 
-@router.post("/comments/", status_code=status.HTTP_201_CREATED)
-async def create_comment_route(comment: CommentCreate,
-                         db: Session = Depends(get_db)
+@router.post("/{blog_id}", status_code=status.HTTP_201_CREATED)
+async def create_comment_for_blog(blog_id: int,
+                                  comment: CommentCreate,
+                                  db: Session = Depends(get_db)
 ):
-    post = db.execute(select(Blogs).where(Blogs.id == comment.blog_id)).scalar_one_or_none()
+    new_comment = create_comment(db, blog_id, comment)
+    return new_comment
+
+
+@router.get("/{blog_id}", response_model=CommentRead)
+async def read_comments_on_blog(blog_id: int, db: Session = Depends(get_db)):
+    post = db.execute(select(Blogs).where(Blogs.id == blog_id, Blogs.status==BlogStatus.PUBLISHED)).scalar_one_or_none()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail="Blog does not exist")
-    comment_service = create_comment(db, comment)
-    return comment_service
-
-
-@router.get("/blog/{blog_id}/comment", response_model=CommentRead)
-async def read_comments_on_blog(blog_id: int, db: Session = Depends(get_db)):
-    blog = db.execute(select(Blogs).where(Blogs.id == blog_id)).scalar_one_or_none()
-    if not blog:
-        return HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                              detail="Blog not found")
-    return blog
+    comments = get_comment_by_blog_id(db, blog_id) 
+    return comments
