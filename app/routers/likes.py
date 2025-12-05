fe8659ce6b7a71fb5c5209c952a97f5654559a43
Bismark Aikins <aikinsb97@gmail.com>
2025-12-05 17:50:55 +0000

    removed all unuse imports

--- app/routers/likes.py
+++ app/routers/likes.py
@@ -1,39 +1,31 @@
-from fastapi import APIRouter, Depends, HTTPException, status, Query
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy import select
 from sqlalchemy.orm import Session
-from ..models import Likes, Blogs
-from ..crud import create_like_post
+from ..models import Like, Blog
+from ..crud import create_like
 from ..schemas import LikePost, BlogStatus
-from ..dependencies import get_db
+from ..dependencies import get_db                                   
 
 
-router = APIRouter(prefix="/likes", tags=["likes"])
+router = APIRouter(prefix="/like", tags=["likes"])
 
 
 @router.post("/{blog_id}", status_code=status.HTTP_201_CREATED)
-async def like_post(blog_id: int,
-                    like: LikePost, 
-                    db: Session = Depends(get_db), 
-                    direction: int = Query(..., le=1)
-):
-    post = db.execute(select(Blogs).where(Blogs.id == blog_id, Blogs.status==BlogStatus.PUBLISHED)).scalar_one_or_none()
+async def create_like_on_blog(blog_id: int, like: LikePost, db: Session = Depends(get_db)):
+    post = db.execute(select(Blog).where(Blog.id == blog_id, Blog.status==BlogStatus.PUBLISHED)).scalar_one_or_none()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail = "Blog not found")
-    like_query = db.execute(select(Likes).where(Likes.id == blog_id, Likes.user_id==like.user_id))
-    found_like = like_query.scalar_one_or_none()
-    if direction == 1:
-        if found_like:
-            raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                                detail="User has already liked post")
-        new_like = create_like_post(db, like, blog_id)
-        db.add(new_like)
-        db.commit()
-        return {"message": "Successfully added like"}
-    else:
-        if not found_like:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                                detail="vote does not exist")
-        like_query.delete(synchronize_session=False)
-        db.commit()
-        return {"message": "Successfully deleted like"}
+                             detail="Blog not found")
+    new_like = create_like(db, like, blog_id)
+    return new_like
+
+
+@router.delete("/delete{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def remove_like(id: int, db: Session = Depends(get_db)):
+    like = db.execute(select(Like).where(Like.id == id)).scalar_one_or_none()
+    if not like:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detil="Like not found")
+    db.delete(like)
+    db.commit()
+    return {"message": "Like removed successfully"}
