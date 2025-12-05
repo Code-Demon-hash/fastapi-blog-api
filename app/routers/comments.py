bf207d6039e53d376927303a4aea38cdfbafe6b8
Bismark Aikins <aikinsb97@gmail.com>
2025-12-05 17:51:40 +0000

    removed all unuse imports

--- app/routers/comments.py
+++ app/routers/comments.py
@@ -1,16 +1,14 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from typing import List
 from sqlalchemy import select
 from sqlalchemy.orm import Session
-from ..models import Blogs, Comments
-from .security.user_authentication import get_current_user
+from ..models import Blog
 from ..schemas import CommentCreate, CommentRead, BlogStatus
 from ..crud import create_comment, get_comment_by_blog_id
 from ..dependencies import get_db
 
 
 
-router = APIRouter(prefix="/comments", tags=["comments"])
+router = APIRouter(prefix="/comment", tags=["comments"])
 
 
 @router.post("/{blog_id}", status_code=status.HTTP_201_CREATED)
@@ -24,7 +22,7 @@ async def create_comment_for_blog(blog_id: int,
 
 @router.get("/{blog_id}", response_model=CommentRead)
 async def read_comments_on_blog(blog_id: int, db: Session = Depends(get_db)):
-    post = db.execute(select(Blogs).where(Blogs.id == blog_id, Blogs.status==BlogStatus.PUBLISHED)).scalar_one_or_none()
+    post = db.execute(select(Blog).where(Blog.id == blog_id, Blog.status==BlogStatus.PUBLISHED)).scalar_one_or_none()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                              detail="Blog not found")
